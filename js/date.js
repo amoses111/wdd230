56a1f3247851e9e73ebf9ff0dddc617ecbd1db7d
amoses111 <mos15018@byui.edu>
2022-04-26 17:59:14 -0600

    last modified java

--- js/date.js
+++ js/date.js
@@ -4,9 +4,9 @@ const year = date.getFullYear();
 
 document.querySelector('#year').innerHTML = year;
 
+const modified = new Date(document.lastModified);
 
-
-
+document.querySelector('#modified').innerHTML = modified;
 
 
 
