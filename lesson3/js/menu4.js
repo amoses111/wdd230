2f1d23de6ff6c1a558c06f0a080dee8250264bfa
amoses111 <mos15018@byui.edu>
2022-05-12 21:36:21 -0600

    js

--- lesson3/js/menu4.js
+++ lesson3/js/menu4.js
@@ -1,3 +1,5 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
