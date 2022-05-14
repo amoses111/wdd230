43c237b77aaff0b99ee3abf88c4b12c0bee5b63c
amoses111 <mos15018@byui.edu>
2022-05-13 22:48:43 -0600

    js

---
+++ chamber/js/date4.js
@@ -0,0 +1,8 @@
+const d = new Date();
+const year = d.getFullYear();
+
+document.querySelector('#year').innerHTML = year;
+
+const modified = document.lastModified;
+
+document.querySelector('#modified').textContent = modified;
