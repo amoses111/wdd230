9dd5cf7d0ee130de5f947986defcf82a50498385
amoses111 <mos15018@byui.edu>
2022-05-14 20:04:02 -0600

    css

--- chamber/js/date4.js
+++ chamber/js/date4.js
@@ -6,3 +6,15 @@ document.querySelector('#year').innerHTML = year;
 const modified = document.lastModified;
 
 document.querySelector('#modified').textContent = modified;
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
