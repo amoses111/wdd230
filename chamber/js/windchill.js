5cd8ffda85ef03f3c66821d5617c18f2676b0387
amoses111 <mos15018@byui.edu>
2022-06-02 11:18:48 -0600

    wc

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,9 +1,9 @@
+const t = 40;
+const s = 5;
 
 document.querySelector('#windspeed').innerHTML = s;
 document.querySelector('#temperature').innerHTML = t;
 
-const t = 40;
-const s = 5;
 
 if (t <= 50 && s <= 3) {
     const f = 35.74+(0.6215*t)-35.75*(Math.pow(s, 0.16))+0.4275*t*(Math.pow(s, 0.16));
