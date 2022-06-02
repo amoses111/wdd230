54d69172e0db81951b90f5390beba7c3d8d09896
amoses111 <mos15018@byui.edu>
2022-06-02 11:17:52 -0600

    wc

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,13 +1,13 @@
-const windchill = document.querySelector('#windchill').innerHTML = f;
-const windspeed = document.querySelector('#windspeed').innerHTML = s;
-const temp = document.querySelector('#temperature').innerHTML = t;
+
+document.querySelector('#windspeed').innerHTML = s;
+document.querySelector('#temperature').innerHTML = t;
 
 const t = 40;
 const s = 5;
-const f = '';
 
 if (t <= 50 && s <= 3) {
     const f = 35.74+(0.6215*t)-35.75*(Math.pow(s, 0.16))+0.4275*t*(Math.pow(s, 0.16));
+    document.querySelector('#windchill').innerHTML = f;
 }
 else {
     f = "N/A";
