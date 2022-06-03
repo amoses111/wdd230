fed36d1023e5e2ed316e6309373bd67b790b2750
amoses111 <mos15018@byui.edu>
2022-06-02 18:23:27 -0600

    java

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,26 +1,38 @@
-const list = document.querySelector('list');
-const input = document.querySelector('favchap');
-const button = document.querySelector('addbutton');
+const list = document.querySelector('#list');
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#addbutton');
 
 button.addEventListener('click', function() {
+    
     const myItem = input.value;
     input.value = '';
 
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
-    
-    listText.textContent = myItem;
-    listBtn.textContent = 'X';
-    list.appendChild(listItem);
-    listItem.appendChild(listText);
-    listItem.appendChild(listBtn);
-    
+    if (myItem !== '') {
+        
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+        
+        listText.textContent = myItem;
+        listBtn.textContent = 'X';
+        list.appendChild(listItem);
+        listItem.appendChild(listText);
+        listItem.appendChild(listBtn);
+        
 
-    listBtn.addEventListener('click', function() {
-        list.removeChild(listItem);
-    });
+        listBtn.addEventListener('click', function() {
+            list.removeChild(listItem);
+        });
 
-    input.focus();
+        input.focus();
+
+    }
+
+    else {
+        myItem = '';
+    }
 });
+
+    
+    
 
