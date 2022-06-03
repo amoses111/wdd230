d681f585dd0794d7c20ac2518ab6fa659bcc2a8f
amoses111 <mos15018@byui.edu>
2022-06-02 18:44:26 -0600

    append

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,6 +1,9 @@
 const list = document.querySelector('#list');
 const input = document.querySelector('#favchap');
 const button = document.querySelector('#addbutton');
+const listItem = document.createElement("li");
+const listText = document.createElement("span");
+const listBtn = document.createElement("button");
 
 button.addEventListener('click', function() {
     
@@ -8,21 +11,17 @@ button.addEventListener('click', function() {
     input.value = '';
 
     if (myItem !== '') {
-
-        const listItem = document.createElement("li");
-        const listText = document.createElement("span");
-        const listBtn = document.createElement("button");
         
         listText.textContent = myItem;
         listBtn.textContent = 'X';
-        /*list.appendChild(listItem);*/
-        listItem.appendChild(listText);
-        listItem.appendChild(listBtn);
+        list.appendChild(listItem);
+        /*listItem.appendChild(listText);
+        listItem.appendChild(listBtn);*/
         
 
-        listBtn.addEventListener('click', function() {
+        /*listBtn.addEventListener('click', function() {
             list.removeChild(listItem);
-        });
+        });*/
 
         input.focus();
 
