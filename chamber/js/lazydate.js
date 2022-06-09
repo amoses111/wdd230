f3838c15c5893af0fc130864c70ad05d08c7a475
amoses111 <mos15018@byui.edu>
2022-06-08 22:31:44 -0600

    date

---
+++ chamber/js/lazydate.js
@@ -0,0 +1,27 @@
+const y = {year: 'numeric'}
+document.getElementById('year').textContent = new Date().toLocaleDateString('en-US', y);
+
+const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.getElementById('currenttime').textContent = new Date().toLocaleDateString('en-US',options);
+
+const date = {weekday: "long", day: 'numeric', month: "long", year: "numeric"};
+document.getElementById('date').textContent = new Date().toLocaleDateString('en-US', date);
+
+//locale storage and visits script
+
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".days");
+
+let numVisits = Number(window.localStorage.getItem("days-ls"));
+
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `1`;
+}
+
+numVisits++;
+localStorage.setItem("days-ls", numVisits);
+
+todayDisplay.textContent = Date.now();
