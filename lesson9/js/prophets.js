e011a10d7cf97a8c50411e6b6e7f5630638e923c
amoses111 <mos15018@byui.edu>
2022-06-18 13:34:23 -0600

    json

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,19 +6,35 @@ fetch(requestURL)
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject);
+        //console.table(jsonObject);
+        const prophets = jsonObject['prophets'];
         prophets.forEach(displayProphets);
     });
 
-const prophets = jsonObject['prophets'];
 
 function displayProphets(prophet) {
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let pa = document.createElement('p');
+    let pb = document.createElement('p')
     let portrait = document.createElement('img');
 
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    pa.textContent = `Date of Birth: ${prophet.birthdate}`;
+    pb.textContent = `Place of Birth: ${prophet.birthplace}`;
 
+    portrait.setAttribute('src', prophet.imageur1);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}, Latter-day President number ${prophet.order}`);
+    portrait.setAttribute('loading', 'lazy');
+
+    card.appendChild(h2);
+    card.appendChild(pa);
+    card.appendChild(pb);
+    card.appendChild(portrait);
+
+
+    document.querySelector('div.cards').appendChild(card);
+    
     
 }
 
