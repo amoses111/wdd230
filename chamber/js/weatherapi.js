289da52ccc1c7cb614dc1deea8d62756303ef547
amoses111 <mos15018@byui.edu>
2022-06-22 20:52:24 -0600

    weatherapi

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,22 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=32617c259650e10eaa9bd75c5735e050`;
+
+apiFetch(url)
+
+async function apiFetch(apiURL) {
+    try {
+        const response = await fetch(apiURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
