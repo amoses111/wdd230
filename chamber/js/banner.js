e76dcdffdb9f68b2d91640d2a9be201e1e6b8a1c
amoses111 <mos15018@byui.edu>
2022-07-01 15:58:47 -0600

    css

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,11 +1,9 @@
 let date = new Date();
-const meet = date.getDay(4);
+const meet = date.getDay();
 const quote = 'Come join us for our meet and greet!';
 
 if (meet == 4 || meet == 2) {
-    let header = document.getElementById('header');
-    let span = document.createElement('span');
-    span.setAttribute('id', 'meet');
-    insertAfter(span, header);
     document.querySelector('#meet').innerHTML = quote;
-} 
+} else {
+    document.getElementById('meet').style.visibility = "hidden";
+}
