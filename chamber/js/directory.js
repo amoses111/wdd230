dbb51cfe4f1ce5be55bc5885870812b70905cf3f
amoses111 <mos15018@byui.edu>
2022-06-30 23:34:47 -0600

    css

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -14,11 +14,17 @@ fetch(requestURL)
 
 function displayCompanies(business) {
     let card = document.createElement('section');
+    card.setAttribute("id", "directorysection")
     let h2 = document.createElement('h2');
+    h2.setAttribute("id", "directoryh2")
     let pa = document.createElement('p');
+    pa.setAttribute("id", "directorypa")
     let pb = document.createElement('p');
+    pb.setAttribute("id", "directorypb")
     let pc = document.createElement('p');
+    pc.setAttribute("id", "directorypc")
     let portrait = document.createElement('img');
+    portrait.setAttribute("id", "directoryimg")
 
     h2.textContent = `${business.name}`;
     pa.textContent = `Address: ${business.address}`;
