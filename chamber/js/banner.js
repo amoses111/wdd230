7f7b4d11ddf0fcbaff0a4c96fc45d09a55bf085a
amoses111 <mos15018@byui.edu>
2022-07-01 15:37:36 -0600

    meet id

--- chamber/js/banner.js
+++ chamber/js/banner.js
@@ -1,7 +1,11 @@
 let date = new Date();
-const meet = date.getDay();
+const meet = date.getDay(4);
 const quote = 'Come join us for our meet and greet!';
 
 if (meet == 4 || meet == 2) {
+    let header = document.getElementById('header');
+    let span = document.createElement('span');
+    span.setAttribute('id', 'meet');
+    insertAfter(span, header);
     document.querySelector('#meet').innerHTML = quote;
-}
+} 
