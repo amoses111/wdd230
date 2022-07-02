47b1e1fe63f56746db14efa7eb0ff76c61102ddf
amoses111 <mos15018@byui.edu>
2022-07-01 18:49:18 -0600

    js

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,5 +1,9 @@
 const requestURL = 'json/data.json';
 const cards = document.querySelector('.cards');
+const directorydisplay = document.querySelector("article");
+const gbutton = document.querySelector("#grid");
+const lbutton = document.querySelector("#list");
+
 
 fetch(requestURL)
     .then(function (response) {
@@ -10,6 +14,36 @@ fetch(requestURL)
         const companies = jsonObject['companies'];
         companies.forEach(displayCompanies);
     });
+
+gbutton.addEventListener("click", () => {
+    display.name = "gdirectory";
+    cards.innerHTML = "";
+    fetch(requestURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (jsonObject) {
+    console.table(jsonObject);  
+    const directory = jsonObject['companies'];
+    directory.forEach(displayDirectory);
+    
+    });
+});
+    
+lbutton.addEventListener("click", () => {
+    display.name = "ldirectory";
+    cards.innerHTML = "";
+    fetch(requestURL)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); 
+    const directory = jsonObject['companies'];
+    directory.forEach(displayDirectory);
+    
+    });
+});
 
 
 function displayCompanies(business) {
@@ -35,18 +69,18 @@ function displayCompanies(business) {
     portrait.setAttribute('alt', `Image of ${business.name}, image number ${business.order}`);
     portrait.setAttribute('loading', 'lazy');
 
-    card.appendChild(h2);
-    card.appendChild(portrait);
-    card.appendChild(pa);   
-    card.appendChild(pb);
-    card.appendChild(pc);
-    
+    if (display.name == "gdirectory") {
+        card.appendChild(h2);
+        card.appendChild(portrait);
+        card.appendChild(pa);   
+        card.appendChild(pb);
+        card.appendChild(pc);
+    } else {
+        card.appendChild(h2);
+        card.appendChild(pa);
+        card.appendChild(pb);
+        card.appendChild(pc);
+    }
 
-
-    document.querySelector('div.cards').appendChild(card);
-
-    
 };
-
-
-
+ 
