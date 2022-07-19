144d42c001d6e6e298a7ebb8796168c5fb0d5875
amoses111 <mos15018@byui.edu>
2022-07-19 13:09:12 -0600

    nav

---
+++ Temple/js/menu.js
@@ -0,0 +1,9 @@
+//MENU
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation');
+
+menubutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+//END - MENU
